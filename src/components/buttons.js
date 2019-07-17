f5e35fff67444805c356235aae4a1b5357c25854
bluelogix <bluelogix010@gmail.com>
2019-07-17 01:40:09 -0400

    speed buttons added and randomizer

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -13,15 +13,22 @@ class Buttons extends React.Component {
             <div className="buttons">
                 <button onClick={this.props.startButton}>Start</button>
                 <button onClick={this.props.stopButton}>Stop</button>
-                {/* <button>Pause</button> */}
                 <button onClick={this.props.clearBox}>Clear</button>
-
-                <select onSelect={this.handleSelect}>
-                    <option >Grid Size</option>
+                <button onClick={this.props.randomButton}>Randomize</button>
+               
+                    
+                    <button onClick={this.props.fastSpeed}>Fast</button>
+                    <button onClick={this.props.slowSpeed}>Slow</button>
+                
+                {/* <select onSelect={this.handleSelect}>
+                    <option>Grid Size</option>
                     <option className="1">15x15</option>
                     <option className="2">30x30</option>
                     <option className="3">45x45</option>
-                </select>
+                </select> */}
+                {/* <form onSelect={this.handleSelect}>
+                    <input>1</input>
+                </form> */}
             </div>
         )
     }    
